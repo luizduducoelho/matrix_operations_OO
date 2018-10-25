577036daeac23719e71c0c853b73b705f571c783
Luiz Coelho <lelcoelho@yahoo.com.br>
2018-10-25 18:06:31 -0300

    Initial files

---
+++ matriz.cpp
@@ -0,0 +1,9 @@
+#include "matriz.h"
+
+int Matriz::get_rows(){
+	return rows;
+}
+
+int Matriz::get_cols(){
+	return cols;
+}
---
+++ matriz.h
@@ -0,0 +1,15 @@
+#ifndef MATRIZ_H
+#define MATRIZ_H
+
+class Matriz{
+private:
+	int rows;
+	int cols;
+	
+public:
+	int get_rows();
+	int get_cols();
+
+};
+
+#endif
