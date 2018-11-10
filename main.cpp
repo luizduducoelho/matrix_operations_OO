2c6dd30add08c9b2ce751dcf55d77a85b9ffbca1
Luiz Coelho <lelcoelho@yahoo.com.br>
2018-11-10 00:30:27 -0200

    Functions on main

--- main.cpp
+++ main.cpp
@@ -6,13 +6,28 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	Matriz X(3,5,1), A(5,5,1), C(5,5,4.), D(1,1), E(1,1), F(1,1), G(1,1), H(1,1), I(1,1) , J(3,3);
-	Matriz B(A);
-
-	A *= 5;
-	X(2,4) = 3;
-
-	cout << A << endl << X;
+	Matriz Y;
+	Matriz X(3,1), A(3,3), C(3,3);
+	Matriz W = C;
+	Matriz Z(A);
+	int numeroLinhas = A.get_rows(); 
+	int numeroColunas = A.get_cols(); 
+	Y.unit();                                     // inicializa Y com a matriz identidade 
+	A(2,1)=10;                               // altera o valor de uma posição de A 
+	Y.zeros();                                // modifica todos os elementos de Y para o valor zero 
+	C=A+A;                                  // Soma 
+	C-=A;                                     // Subtração  
+	A=C-A;                                  // Subtração 
+	A+=A;                                   // Soma 
+	A=~C;                                  // A é igual a transposta de C 
+	X.ones();                             // modifica todos os elementos de X com 1s
+	X*=2;                                   // multiplicação por uma constante 
+	//C=A*X;                                         // multiplicação de matrizes 
+	C*=X;                                            // multiplicação de matrizes 
+	if (A == C)                                 // verifica a igualdade entre A e C 
+	if(X != Y)                                  // verifica a desigualdade entre A e C 
+	cout << C << endl;                        // Impressão de matrizes 
+	cin >> Y;                                     // leitura de dados para dentro da matriz Y 
 
 	return 0;
 }
